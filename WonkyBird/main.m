bec71dabaa86ba86875130d87f4a00d81c46a54f
Cam Saul <cam@getluckybird.com>
2014-02-23 16:57:21 -0800

    Initial Commit

---
+++ WonkyBird/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WonkyBird
+//
+//  Created by Cam Saul on 2/23/14.
+//  Copyright LuckyBird, Inc. 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
