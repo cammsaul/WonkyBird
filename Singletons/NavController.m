6ecfa5afcdc06abe0152d3d5330f11c811eaaf0e
Cam Saul <cam@geotip.com>
2014-02-28 16:36:29 -0800

    iAD integrated

--- Singletons/NavController.m
+++ Singletons/NavController.m
@@ -6,34 +6,41 @@
 //  Copyright (c) 2014 LuckyBird, Inc. All rights reserved.
 //
 
+#import <iAd/iAd.h>
+
+
 #import "NavController.h"
 #import "MainScene.h"
 
 @implementation NavController
-
-// The available orientations should be defined in the Info.plist file.
-// And in iOS 6+ only, you can override it in the Root View controller in the "supportedInterfaceOrientations" method.
-// Only valid for iOS 6+. NOT VALID for iOS 4 / 5.
 -(NSUInteger)supportedInterfaceOrientations {
 	return UIInterfaceOrientationMaskPortrait;
 }
 
-// Supported orientations. Customize it for your own needs
-// Only valid on iOS 4 / 5. NOT VALID for iOS 6.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
 	return NO;
 }
-
-// This is needed for iOS4 and iOS5 in order to ensure
-// that the 1st scene has the correct dimensions
-// This is not needed on iOS6 and could be added to the application:didFinish...
 - (void)directorDidReshapeProjection:(CCDirector*)director
 {
 	if(director.runningScene == nil) {
 		// Add the first scene to the stack. The director will draw it immediately into the framebuffer. (Animation is started automatically when the view is displayed.)
 		// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
 		[director runWithScene: [MainScene node]];
+		
+		// add an iad view
+		auto adBanner = [[ADBannerView alloc] initWithAdType:ADAdTypeBanner];
+		adBanner.translatesAutoresizingMaskIntoConstraints = NO;
+		[self.view addSubview:adBanner];
+		
+		NSMutableArray *constraints = [NSMutableArray array];
+		[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"|[adBanner]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(adBanner)]];
+		[constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[adBanner(50)]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(adBanner)]];
+		for (NSLayoutConstraint *c in constraints) {
+			c.priority = UILayoutPriorityRequired;
+		}
+		[self.view addConstraints:constraints];
 	}
 }
+
 @end
